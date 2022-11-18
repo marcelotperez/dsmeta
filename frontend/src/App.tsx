bb18e3d520c27079891f6ca437fc085e518ab923
Marcelo Perez <marcelotperez@gmail.com>
2022-11-18 09:25:14 -0400

    front end

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
+import NotificationButton from "./components/NotificationButtun"
 
 function App() {
-  return (<h1>Olá Mundo</h1>)
+  return (
+    <>
+    <h1>Olá Mundo</h1>
+    <NotificationButton />
+    <NotificationButton />
+    </>
+  )
 }
 
 export default App
